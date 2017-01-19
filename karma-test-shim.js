6876cdfbfc0206b26556171d31a8dd28c47f9f62
Joppe Aarts <joppe@apestaartje.info>
2017-01-19 22:37:28 +0100

    initila commit

---
+++ karma-test-shim.js
@@ -0,0 +1,41 @@
+Error.stackTraceLimit = 0;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+
+var builtPaths = (__karma__.config.builtPaths || ['dist/']).map(function (p) {
+    return '/base/' + p;
+});
+
+__karma__.loaded = function () {
+};
+
+function isJsFile(path) {
+    return path.slice(-3) == '.js';
+}
+
+function isSpecFile(path) {
+    return /\.spec\.(.*\.)?js$/.test(path);
+}
+
+function isBuiltFile(path) {
+    return isJsFile(path) && builtPaths.reduce(function (keep, bp) {
+        return keep || (path.substr(0, bp.length) === bp);
+    }, false);
+}
+
+var allSpecFiles = Object.keys(window.__karma__.files)
+    .filter(isSpecFile)
+    .filter(isBuiltFile);
+
+SystemJS.config({
+    baseURL: 'base'
+});
+
+System.import('system.conf.js').then(initTesting);
+
+function initTesting () {
+    return Promise.all(
+        allSpecFiles.map(function (moduleName) {
+            return System.import(moduleName);
+        })
+    ).then(__karma__.start, __karma__.error);
+}
