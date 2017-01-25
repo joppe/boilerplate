c4811dc20a27499c31e735d76f15aa90895ea295
Joppe Aarts <joppe@apestaartje.info>
2017-01-25 22:40:49 +0100

    karma test shim restructured

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -2,23 +2,61 @@ Error.stackTraceLimit = 0;
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
 
-__karma__.loaded = function () {
-};
+// Don't trigger load event, we do that later when all spec files are imported
+__karma__.loaded = function () {};
 
+/**
+ * Check if this is a path to a JavaScript file
+ *
+ * @param {string} path
+ * @returns {boolean}
+ */
 function isJsFile(path) {
     return path.slice(-3) == '.js';
 }
 
+/**
+ * Check if this is path to a spec file
+ *
+ * @param {string} path
+ * @returns {boolean}
+ */
 function isSpecFile(path) {
     return /\.spec\.(.*\.)?js$/.test(path);
 }
 
-function isBuiltFile(path, builtPaths) {
-    return isJsFile(path) && builtPaths.reduce(function (keep, bp) {
-        return keep || (path.substr(0, bp.length) === bp);
-    }, false);
+/**
+ * Check if this path is a JavaScript file and part of the built path
+ *
+ * @param {string} path
+ * @param {string} builtPath
+ * @returns {boolean}
+ */
+function isBuiltFile(path, builtPath) {
+    return isJsFile(path) && (path.substr(0, builtPath.length) === builtPath);
 }
 
+/**
+ * Get all spec files that are in this built path
+ *
+ * @param {string[]} files
+ * @param {string} builtPath
+ * @returns {string[]}
+ */
+function getSpecFiles(files, builtPath) {
+    return Object.keys(files)
+        .filter(isSpecFile)
+        .filter(function (path) {
+            return isBuiltFile(path, builtPath);
+        });
+}
+
+/**
+ * Import all spec files, when done trigger karma start
+ *
+ * @param {string[]}allSpecFiles
+ * @returns {Promise}
+ */
 function initTesting(allSpecFiles) {
     return Promise.all(
         allSpecFiles.map(function (moduleName) {
@@ -28,18 +66,13 @@ function initTesting(allSpecFiles) {
 }
 
 (function () {
-    var builtPaths = ['/base'],
-        allSpecFiles= Object.keys(window.__karma__.files)
-            .filter(isSpecFile)
-            .filter(function (path) {
-                return isBuiltFile(path, builtPaths);
-            });
+    var base = 'base';
 
     SystemJS.config({
-        baseURL: 'base'
+        baseURL: base
     });
 
     System.import('system.conf.js').then(function () {
-        initTesting(allSpecFiles);
+        initTesting(getSpecFiles(window.__karma__.files, '/' + base));
     });
 }());
