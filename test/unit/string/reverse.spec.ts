6eddf9d2ff433ab1ada5b3e12a28b1da5c6f2f6e
Joppe Aarts <joppe@apestaartje.info>
2017-01-25 22:42:28 +0100

    removed test/sample scripts

--- test/unit/string/reverse.spec.ts
+++
@@ -1,8 +0,0 @@
-import {reverse} from '../../../dist/reverse';
-
-describe('reverse', ():void => {
-    it('reverse a string', ():void => {
-        expect(reverse('hello world')).toBe('dlrow olleh');
-        expect(reverse(reverse('hello world'))).toBe('hello world');
-    });
-});
