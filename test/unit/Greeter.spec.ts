e02ee6839eaf16aec30d26eda8d4e49831b33b08
Joppe Aarts <joppe@apestaartje.info>
2017-02-07 19:39:49 +0100

    remove sample files

--- test/unit/Greeter.spec.ts
+++
@@ -1,9 +0,0 @@
-import {Greeter} from './../../src/Greeter';
-
-describe('Greeter', ():void => {
-    it('greet', ():void => {
-        const g:Greeter = new Greeter('Joppe');
-
-        expect(g.greet()).toBe('Hello Joppe');
-    });
-});
