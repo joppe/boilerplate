061227705a90872ac40254708a62e21434ebb99c
Joppe Aarts <joppe@apestaartje.info>
2017-08-21 22:06:01 +0200

    new karma config based no longer based on webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,175 +1,80 @@
-/**
- * @author: @AngularClass
- */
+/*global module*/
 
 module.exports = function (config) {
-    const path = require('path');
-    const webpackConfig = require('./webpack.config.js');
-
-    delete webpackConfig.entry;
-    delete webpackConfig.output;
-
-    webpackConfig.devtool = 'inline-source-map';
-    webpackConfig.module.rules = [
-        /**
-         * Source map loader support for *.js files
-         * Extracts SourceMaps for source files that as added as sourceMappingURL comment.
-         *
-         * See: https://github.com/webpack/source-map-loader
-         */
-        {
-            enforce: 'pre',
-            test: /\.js$/,
-            loader: 'source-map-loader'
-        },
-
-        {
-            test: /\.ts$/,
-            use: [
-                {
-                    loader: 'ts-loader',
-                    query: {
-                        // use inline sourcemaps for "karma-remap-coverage" reporter
-                        sourceMap: false,
-                        inlineSourceMap: true,
-                        compilerOptions: {
-                            // Remove TypeScript helpers to be injected
-                            // below by DefinePlugin
-                            removeComments: true,
-                            target: 'es5'
-                        }
-                    }
-                }
-            ]
-        },
-
-        /**
-         * Instruments JS files with Istanbul for subsequent code coverage reporting.
-         * Instrument only testing sources.
-         *
-         * See: https://github.com/deepsweet/istanbul-instrumenter-loader
-         */
-        {
-            enforce: 'post',
-            test: /\.(js|ts)$/,
-            loader: 'istanbul-instrumenter-loader',
-            include: path.resolve('src'),
-            exclude: [
-                /\.(spec)\.ts$/,
-                /node_modules/
-            ]
-        }
-
-    ];
-
     const configuration = {
+        frameworks: ['jasmine', 'karma-typescript'],
 
-        // base path that will be used to resolve all patterns (e.g. files, exclude)
-        basePath: '',
-
-        /*
-         * Frameworks to use
-         *
-         * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-         */
-        frameworks: ['jasmine'],
-
-        // list of files to exclude
-        exclude: [],
-
-        client: {
-            captureConsole: false
-        },
-
-        /*
-         * list of files / patterns to load in the browser
-         *
-         * we are building the test environment in ./spec-bundle.js
-         */
         files: [
             {
-                pattern: './spec-bundle.js',
-                watched: false
+                pattern: 'src/**/*.ts'
+            },
+            {
+                pattern: 'test/**/*.ts'
             }
         ],
 
-        /*
-         * preprocess matching files before serving them to the browser
-         * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-         */
         preprocessors: {
-            './spec-bundle.js': [
-                'coverage',
-                'webpack',
-                'sourcemap'
-            ]
+            'src/**/*.ts': ['karma-typescript'],
+            'test/**/*.ts': ['karma-typescript']
         },
 
-        // Webpack Config at ./webpack.test.js
-        webpack: webpackConfig,
-
-        coverageReporter: {
-            type: 'in-memory'
-        },
-
-        remapCoverageReporter: {
-            'text-summary': null,
-            json: './coverage/coverage.json',
-            html: './coverage/html'
-        },
-
-        // Webpack please don't spam the console when running in karma!
-        webpackMiddleware: {
-            // webpack-dev-middleware configuration
-            // i.e.
-            noInfo: true,
-
-            // and use stats to turn off verbose output
-            stats: {
-                // options i.e.
-                chunks: false
-            }
-        },
-
-        /*
-         * test results reporter to use
-         *
-         * possible values: 'dots', 'progress'
-         * available reporters: https://npmjs.org/browse/keyword/karma-reporter
-         */
         reporters: [
             'progress',
-            'coverage',
-            'remap-coverage'
+            'karma-typescript'
         ],
 
-        // web server port
+        karmaTypescriptConfig: {
+            coverageOptions: {
+                exclude: [
+                    /\.(d|spec|test)\.ts$/i,
+                    /node_modules/i
+                ]
+            },
+            exclude: ['demo'],
+            include: [
+                'src/**/*',
+                'test/**/*'
+            ],
+            reports: {
+                clover: {
+                    directory: 'coverage',
+                    subdirectory: 'clover',
+                    filename: 'coverage.xml'
+                },
+                'html': {
+                    directory: 'coverage',
+                    subdirectory: 'html',
+                    filename: 'coverage'
+                }
+            },
+            tsconfig: './tsconfig.json'
+
+        },
+
         port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
         colors: true,
 
-        /*
-         * level of logging
-         * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-         */
         logLevel: config.LOG_WARN,
 
-        // enable / disable watching file and executing tests whenever any file changes
         autoWatch: false,
 
-        /*
-         * start these browsers
-         * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-         */
         browsers: [
-            'PhantomJS'
+            'ChromeNoSandboxHeadless'
         ],
 
-        /*
-         * Continuous Integration mode
-         * if true, Karma captures browsers, runs the tests and exits
-         */
+        customLaunchers: {
+            ChromeNoSandboxHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    ' --remote-debugging-port=9222'
+                ]
+            }
+        },
+
         singleRun: true
     };
 
