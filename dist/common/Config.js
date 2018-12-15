3652bd9bc526f9e7026cee9adcca39c78014591a
Joppe Aarts <joppe@apestaartje.info>
2018-12-15 16:24:37 +0100

    removed compiled files

--- dist/common/Config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPE_LIBRARY = 'Library';
-exports.TYPE_PROJECT = 'Project';
--- dist/common/copy-files.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const copy_file_1 = require("./copy-file");
-function copyFiles(sourcePath, targetPath, files) {
-    files.forEach((file) => {
-        copy_file_1.copyFile(sourcePath, targetPath, file);
-    });
-}
-exports.copyFiles = copyFiles;
